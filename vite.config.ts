3c7cc2323847190e7cf76e2b823bc448ab5f066c
Mahesh Rakte <107369702+Mahesh00234h@users.noreply.github.com>
2025-06-23 13:48:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
      '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://og-backend-mwwi.onrender.com/api',
         changeOrigin: true,
         secure: false,
         ws: false,
