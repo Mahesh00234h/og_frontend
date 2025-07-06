ffe9fd778c80613b4a80e64f998faadd0efc99c7
Mahesh00234h <maheshrakate@gmail.com>
2025-07-07 01:35:31 +0530

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
 
+
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App.tsx';
+import './index.css';
 createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ''}>
+    <App />
+  </GoogleOAuthProvider>
+);
