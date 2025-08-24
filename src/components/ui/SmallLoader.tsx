61faa7840535ee501be7d672f6106e7731df4f6a
Mahesh Rakte <107369702+Mahesh00234h@users.noreply.github.com>
2025-08-25 00:12:15 +0530

    Update and rename ogloader.jsx to SmallLoader.tsx

---
+++ src/components/ui/SmallLoader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './smallloader.css';
+
+const SmallLoader: React.FC = () => {
+  return (
+    <div className="small-loader-container">
+      <div className="small-logo-wrapper">
+        <img
+          src="https://res.cloudinary.com/dxu2xavnq/image/upload/v1750839403/favicon_bcvqnp.png"
+          alt="Small Logo"
+        />
+      </div>
+      <div className="small-loader"></div>
+    </div>
+  );
+};
+
+export default SmallLoader;
