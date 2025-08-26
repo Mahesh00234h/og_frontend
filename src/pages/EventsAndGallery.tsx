83fe8754ff38b61eab907ccc79fab46633912084
Mahesh Rakte <107369702+Mahesh00234h@users.noreply.github.com>
2025-08-26 18:45:14 +0530

    Update EventsAndGallery.tsx

--- src/pages/EventsAndGallery.tsx
+++ src/pages/EventsAndGallery.tsx
@@ -397,7 +397,7 @@ const EventsAndGallery: React.FC = () => {
                       </div>
                       <div className="flex items-center space-x-2">
                         <Users
-                          className=`{h-4 w-4 ${
+                          className={`h-4 w-4 ${
                             isDarkMode ? 'text-cyan-400' : 'text-blue-500'
                           }`}
                         />
